c7fd981c8a455956b54b600025f6b04a45fa8a38
Rúben Carneiro <rubencarneiro01@gmail.com>
2023-01-11 17:31:23 +0000

    # Configure - Change Branding

--- libs/branding.js
+++ libs/branding.js
@@ -1,8 +1,8 @@
 module.exports = function(s,config,lang,app,io){
     if(config.showPoweredByShinobi === undefined){config.showPoweredByShinobi=true}
-    if(config.poweredByShinobi === undefined){config.poweredByShinobi='Powered by Shinobi.Systems'}
+    if(config.poweredByShinobi === undefined){config.poweredByShinobi='Powered by SageTea (R)'}
     if(config.poweredByShinobiClass === undefined){config.poweredByShinobiClass='margin:15px 0 0 0;text-align:center;color:#777;font-family: sans-serif;text-transform: uppercase;letter-spacing: 3;font-size: 8pt;'}
-    if(config.webPageTitle === undefined){config.webPageTitle='Shinobi'}
+    if(config.webPageTitle === undefined){config.webPageTitle='SageTeaViewCCTV'}
     if(config.showLoginCardHeader === undefined){config.showLoginCardHeader=true}
     if(config.webFavicon === undefined){config.webFavicon='libs/img/icon/favicon.ico'}
     if(config.logoLocation76x76 === undefined){config.logoLocation76x76='libs/img/icon/apple-touch-icon-76x76.png'}
